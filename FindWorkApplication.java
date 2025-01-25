65b3337f19ca7fe59b7f7456a0e30e11737d5e4e
Viranga Lakshani <104049331+Viranga-Lakshani@users.noreply.github.com>
2025-01-25 09:34:00 +0530

    Create FindWorkApplication.java

---
+++ FindWorkApplication.java
@@ -0,0 +1,12 @@
+package com.example.findwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindWorkApplication.class, args);
+    }
+}
